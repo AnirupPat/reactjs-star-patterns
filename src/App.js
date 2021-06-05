4939c151a02ef9a59c8ad9b2030e2fe4b4db6b19
Anirup Patnaik <anirup049@gmail.com>
2021-06-05 22:48:47 +0530

    Square Patterns

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import SquarePatterns from './components/SquarePatterns/SquarePatterns';
 
 function App() {
   return (
     <div>
-      
+      <SquarePatterns />
     </div>
       
   );
---
+++ src/components/SquarePatterns/SquarePatterns.js
@@ -0,0 +1,16 @@
+const SquarePatterns = () => {
+    const n = 5;
+    let string = "";
+    for(let i = 0; i < n; i++) {
+        for(let j = 0; j < n; j++) {
+            string += '*';
+        }
+        string += '\n';
+    }
+    console.log(string);
+    return (
+        <div>{string}</div>
+    );
+}
+
+export default SquarePatterns;
